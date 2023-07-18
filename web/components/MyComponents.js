adfb693982d9df2e114f4217a3d50bd09936aaa1
diego-medina-truenorth <diego.medina.truenorth@synapsefi.com>
2023-07-18 14:25:15 -0300

    styles added

---
+++ web/components/MyComponents.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './MyComponent.module.css';
+
+const MyComponent = () => {
+  return <button className={styles.button}>Submit</button>;
+};
+
+export default MyComponent;
