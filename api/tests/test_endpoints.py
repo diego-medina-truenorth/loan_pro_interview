6676cf6a6ed7326050212793b2ca95e0e659f790
diego-medina-truenorth <diego.medina.truenorth@synapsefi.com>
2023-07-19 17:06:52 -0300

    test endpoints

--- api/tests/test_endpoints.py
+++ api/tests/test_endpoints.py
@@ -6,6 +6,10 @@ from app.database import SessionLocal, engine
 from app.authentication import pwd_context
 from app.database import get_db
 
+from fastapi.testclient import TestClient
+
+
+
 @pytest.fixture(scope="module")
 def client():
     with TestClient(app) as test_client:
@@ -38,3 +42,18 @@ def test_login_success(client):
 
     assert "access_token" in response.json()
     access_token = response.json()["access_token"]
+
+client = TestClient(app)
+
+def test_calculator_addition():
+    payload = {"operation_type": "addition", "amount": 5}
+    response = client.post("/calculator/addition", json=payload)
+    assert response.status_code == 200
+    assert response.json() == {"result": 15} 
+
+def test_calculator_subtraction():
+    payload = {"operation_type": "subtraction", "amount": 3}
+    response = client.post("/calculator/subtraction", json=payload)
+    assert response.status_code == 200
+    assert response.json() == {"result": 7}
+
